e76d8612d0df236bb85a1896b36692620937c6ba
chrys <c.beltran@ext.bouygues-immobilier.com>
2021-11-03 11:18:57 +0100

    update

--- bash.sh
+++ bash.sh
@@ -1,10 +1,10 @@
-git init
-dvc init
+#git init
+dvc init -f
 git commit -m "Initialize DVC repo"
 dvc remote add -d dvc-storage /tmp/dvc-storage
 @mkdir data
 
-https://github.com/mlflow/mlflow-example.git
+#https://github.com/mlflow/mlflow-example.git
 
 dvc add data/wine-quality.csv 
 git add data/wine-quality.csv.dvc data/.gitignore
